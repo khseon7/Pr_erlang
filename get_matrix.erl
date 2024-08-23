5dc0f3c4e4e17a185253ec3c66b4c2fc6bd4a2fa
khseon7 <gkrtjs5544@gmail.com>
2024-08-23 17:46:21 +0900

    입력받는 리스트들을 1차원으로 계속 붙이기

--- get_matrix.erl
+++ get_matrix.erl
@@ -1,10 +1,20 @@
 -module(get_matrix).
--export([start/0]).
+-export([for/2,start/0]).
+-import(string,[trim/1,split/3]).
+
+for(0,Term)->
+    Term;
+
+    for(N,Term) when N>0->
+        Input=io:get_line("Enter Row Elements : "),
+        InputTrimmed=trim(Input),
+        StringList=split(InputTrimmed," ",all),
+        IntegerList=[list_to_integer(S) || S<-StringList],
+        io:format("~w~n",[IntegerList]),
+        NewTerm=Term ++ IntegerList,
+        for(N-1,NewTerm).
 
 start() ->
-    Input=io:get_line("Enter numbers : "),
-    InputTrimmed=string:trim(Input),
-    StringList=string:split(InputTrimmed," ",all),
-    IntegerList=[list_to_integer(S) || S<-StringList],
-    io:format("Integer List: ~p~n",[IntegerList]),
-    IntegerList.
+    {ok,[Number]}=io:fread("","~d"),
+    Results=for(Number,[]),
+    io:format("~w~n",[Results]).
