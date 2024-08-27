4ba386d8433eb711856b6b08103d57a72ee075c4
khseon7 <gkrtjs5544@gmail.com>
2024-08-27 20:33:28 +0900

    Implementing an N-second timer and outputting the time in seconds

--- my_timer.erl
+++ my_timer.erl
@@ -6,9 +6,18 @@ start()->
 
     {ok,[N]}=io:fread("","~d"),
 
-    erlang:send_after(N*1000,self(), hello),
+    erlang:send_after(1000,self(),{tick,1}),
 
+    erlang:send_after(N*1000, self(), hello),
+    
+    loop(N).
+
+loop(N)->
     receive
+        {tick, Sec}->
+            io:format("~p second(s) passed~n",[Sec]),
+            erlang:send_after(1000,self(),{tick,Sec+1}),
+            loop(N);
         hello ->
             io:format("Hello, World!~n")
     end.
