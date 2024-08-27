9e281aaa130f7c8e832490f15743b4659ba09e60
khseon7 <gkrtjs5544@gmail.com>
2024-08-27 15:38:37 +0900

    Solving Baekjoon Problem b9012 using recursion

--- baekJoon/b9012.erl
+++ baekJoon/b9012.erl
@@ -1,25 +1,43 @@
 -module(b9012).
--export([for/2,for/3,ps/0,start/0]).
+-export([for/1,for/2,ps/0,start/0]).
+-import(lists,[nth/2,append/2]).
 
-for(List,Term)->
-    for(List,Term,1).
-
-for([],Term,_N)->
+for([],Term)->
     case length(Term) of
         0 -> "YES";
         _ -> "NO"
     end;
     
-    for(List,Term,N) when length(List)>0 ->
-        io:format("1st ~p~n",[lists:nth(N,List)]),
-        NewTerm=lists:append(lists:nth(N,List),Term),
-        NewList=lists:delete(lists:nth(N,List),List),
-        io:format("~p~n",[NewTerm]),
-        for(NewList,NewTerm,N+1).
+    for(List,Term) when length(List)>0 ->
+        N=1,
+        Element=nth(N,List),
+        % io:format("~w's element is ~p~n",[N,Element]),
+        case Element of
+            40->
+                NewTerm=append([Element],Term);
+            41->
+                NewTerm = case length(Term) of
+                    0->append([Element],Term);
+                    _->lists:droplast(Term)
+                end;
+            _->
+                NewTerm=Term
+        end,
+        NewList=lists:delete(Element,List),
+        % io:format("~p~n",[NewTerm]),
+        for(NewList,NewTerm).
 
 ps()->
-    Input=[string:trim(io:get_line(""))],
+    Input=string:trim(io:get_line("")),
+    % io:format("~w, ~p~n",[length(Input),Input]),
     for(Input,[]).
 
+for(1) -> io:format("~p~n",[ps()]);
+for(N) when N>1->
+    io:format("~p~n",[ps()]),
+    for(N-1).
+
+
 start()->
-    ps().
+    {ok, [N]} = io:fread("", "~d"), % {ok, [N]} 형식으로 반환됨 
+    for(N).
